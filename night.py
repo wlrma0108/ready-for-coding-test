925d7c5e16ded99a246b5bbe47f62354b4334c50
kim hojoong <82634705+wlrma0108@users.noreply.github.com>
2025-03-31 13:41:42 +0900

    a

--- night.py
+++ night.py
@@ -1,3 +1,15 @@
 input =input()
 row = int(input[1])
 column = int(input - int(ord('a'))) + 1
+#이거보다 깔끔한 방법 없나?
+step = [(-2,-1),(-1,-2),(1,-2),(2,-1),(2,1),(1,2), (-1,2),(-2,1)]
+
+result = 0
+
+for ste in step:
+    nextr = row + step[0]
+    nextc = column + step[1]
+    # 다른 방법 없나?
+    if ???
+
+print(result)
