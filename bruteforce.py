7b8ecbab4ff3ab298fc95bb6067691ab8f92e337
kim hojoong <82634705+wlrma0108@users.noreply.github.com>
2025-06-11 22:44:29 +0900

    add

--- bruteforce.py
+++ bruteforce.py
@@ -15,3 +15,29 @@ def get_path_to_root(x, parent):
         path.append(x)
         x = parent[x]
     return path  # (자기자신~루트)
+    
+def get_parent(node, K):
+    if K == 1:
+        return node - 1
+    else:
+        return (node - 2) // K + 1 if node != 1 else 1
+
+def brute_force_distance(x, y, K):
+    cnt = 0
+    while x != y:
+        if x > y:
+            x = get_parent(x, K)
+        else:
+            y = get_parent(y, K)
+        cnt += 1
+    return cnt
+
+# 사용 예시:
+N, K, Q = 9, 3, 3  # 입력 예시
+queries = [(8, 9), (5, 7), (8, 4)]
+
+for x, y in queries:
+    if K == 1:
+        print(abs(x - y))
+    else:
+        print(brute_force_distance(x, y, K))
