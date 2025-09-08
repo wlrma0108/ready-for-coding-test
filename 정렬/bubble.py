bdfba37d2e8a489f781848020c5d2d0e3a1fe44f
kim hojoong <82634705+wlrma0108@users.noreply.github.com>
2025-09-08 20:56:13 +0900

    bubble

---
+++ 정렬/bubble.py
@@ -0,0 +1,9 @@
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n - 1):  
+        for j in range(n - 1 - i):  
+            if arr[j] > arr[j + 1]:  
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
+
+print(bubble_sort([5, 2, 9, 1, 5, 6]))
